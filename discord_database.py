f0afa56f28f92edc4d26667fa8ed21aa5135a47c
begilbert-sys <alicornderpy@gmail.com>
2023-04-12 11:57:01 -0700

    fixed bugs

--- discord_database.py
+++ discord_database.py
@@ -1,18 +1,19 @@
 import discord
 
+import datetime
 import emoji
 import json
 import os
 import pytz
 import re
-import time
 
 with open('words.txt') as f:
     WORDS = {w.lower() for w in f.read().split('\n')}
     
 with open('curse_words.txt') as f:
     CURSE_WORDS = set(f.read().split('\n'))
-  
+
+### utility functions 
 def get_emojis(string):
     unicode_emoji_list = emoji.distinct_emoji_list(string)
     custom_emoji_list = re.findall(r'<:\w*:\d*>', string)
@@ -23,6 +24,11 @@ def top_items(dictionary, n):
     return sorted(dictionary, key=lambda x: dictionary[x], reverse=True)[:n]
 
 class Dictionary_Database:
+    '''
+    self.database - a dictionary of users, each with a number of sub-dictionaries keeping track of various statistics
+
+    self.channel_endmsgs : a dictionary which keeps track of the last message that has been processed from each channel
+    '''
     def __init__(self, guild_id: int):
         self.db_file = f'discord_dbs/discord_database_{guild_id}.json'
         if os.path.isfile(self.db_file):
@@ -35,6 +41,7 @@ class Dictionary_Database:
             self.channel_endmsgs = dict()
             
     def save(self):
+        '''stores the collected data in a json file'''
         full_data = {'database': self.database,
                      'channel_endmsgs': self.channel_endmsgs}
         
@@ -42,7 +49,8 @@ class Dictionary_Database:
             json.dump(full_data, f)
             
     def add_user(self, user: discord.user):
-        assert user.id not in self.database
+        user_key = str(user.id)
+        assert user_key not in self.database
 
         default_info_dict = {'tag': f'{user.name}#{user.discriminator}',
                      'channel_counts': dict(),
@@ -54,23 +62,20 @@ class Dictionary_Database:
                      'curse_word_count': 0}
 
         # json keys NEED to be strings
-        self.database[str(user.id)] = default_info_dict
+        self.database[user_key] = default_info_dict
         
     def process_message(self, message: discord.message):
-
-        
-        
         #user
-        user_id = message.author.id
-        if user_id not in self.database:
+        user_key = str(message.author.id)
+        if user_key not in self.database:
             self.add_user(message.author)
             
-        user_dict = self.database[str(user_id)]
+        user_dict = self.database[user_key]
         
         #channel
-        channel_id = str(message.channel.id)
+        channel_key = str(message.channel.id)
         channel_dict = user_dict['channel_counts']
-        channel_dict[channel_id] = channel_dict.get(channel_id, 0) + 1
+        channel_dict[channel_key] = channel_dict.get(channel_key, 0) + 1
 
         #hour
         user_dict['hour_counts'][str(message.created_at.hour)] += 1
@@ -89,7 +94,8 @@ class Dictionary_Database:
         unique_words_dict = user_dict['unique_word_counts']
         
         for word in message.content.split():
-            if word.isalpha():
+            # valid 'words' need to be alphabetic and under 20 characters
+            if word.isalpha() and len(word) < 20:
                 word = word.lower()
                 if word not in WORDS:
                     unique_words_dict[word] = unique_words_dict.get(word, 0) + 1
@@ -118,6 +124,7 @@ class Dictionary_Database:
     
     @property
     def database_totals(self):
+        '''sums up subdictionary totals across all users'''
         totals = dict()
         iterlist = ['channel_counts', 'hour_counts', 'date_counts',
                     'emoji_counts', 'mention_counts', 'unique_word_counts']
@@ -131,3 +138,10 @@ class Dictionary_Database:
     def total_users(self):
         return len(self.database)
     
+    def first_message_date(self):
+        date_string = min(self.database_totals['date_counts'])
+        return datetime.strptime(date_string, '%y%m%d')
+    
+    def last_message_date(self):
+        date_string = max(self.database_totals['date_counts'])
+        return datetime.strptime(date_string, '%y%m%d')
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from discord_database import Dictionary_Database
 
 from API_Key import KEY
 
-GUILD_ID = 100770673609150464
+GUILD_ID = 920955603638190090
 
 MSG_LIMIT = 25
 
@@ -19,27 +19,25 @@ class MyClient(discord.Client):
         
         guild = self.get_guild(GUILD_ID)
         
-        n = 0
         for channel in guild.channels:
             perms = channel.permissions_for(guild.me)
-            if type(channel) is discord.channel.TextChannel and perms.read_message_history:
-                channel_key = str(channel.id)
+            channel_key = str(channel.id)
+            # only iterates thru channels that: are text channels, it has perms to, and the last msg hasn't been processed
+            if (type(channel) is discord.channel.TextChannel and perms.read_message_history 
+                and channel.last_message_id != database.channel_endmsgs.get(channel_key)):
+
+                # setup for message retrieval: allows both new and alread-scraped channels to be scraped
                 kwargs = {'limit': MSG_LIMIT}
                 if channel_key in database.channel_endmsgs:
-                    msg_id = database.channel_endmsgs[channel_key]
-                    print(msg_id)
-                    message_obj = await channel.fetch_message(msg_id)
+                    last_msg_id = database.channel_endmsgs[channel_key]
+                    message_obj = await channel.fetch_message(last_msg_id)
                     kwargs['after'] = message_obj
                 else:
                     kwargs['oldest_first'] = True
-                    
+
                 async for message in channel.history(**kwargs):
-                    n += 1
-                    if n % 1000 == 0:
-                        print(n)
                     if message.type in (discord.MessageType.default, discord.MessageType.reply):
                         database.process_message(message)
-            database.channel_endmsgs[channel_key] = message.id
         print('done!')
         database.save()
         print(database.database_totals['channel_counts'])
