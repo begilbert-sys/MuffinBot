bf5111a6cb8c8e17155c5ce6c7e5c9f523b673b0
begilbert-sys <alicornderpy@gmail.com>
2023-04-14 23:30:51 -0700

    added to html template

--- stats/views.py
+++ stats/views.py
@@ -11,8 +11,13 @@ database = Dictionary_Database()
 # Create your views here.
 def index(request):
     context = {
-        'database': database, 
+        'database': database.database, 
+        'database_totals': database.database_totals,
+
         'first_message_date': database.first_message_date(),
-        'last_message_date': database.last_message_date()
+        'last_message_date': database.last_message_date(),
+        'total_users' : len(database.database),
+        'reporting_period_days': database.total_days()
+
         }
     return render(request, "index.html", context)
