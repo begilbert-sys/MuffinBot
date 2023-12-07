3dda0904e04ba67fd21db306b345c8613045584b
Ben Gilbert <alicornderpy@gmail.com>
2023-12-06 22:33:16 -0800

    finished timezone selector outline

--- stats/static/js/server_setup.js
+++ stats/static/js/server_setup.js
@@ -1,17 +1,4 @@
-function toggleSelector() {
-    var tzPopdown = document.getElementById('timezone-wrapper');
-    var arrow = document.getElementById('popdown-arrow');
-    if (tzPopdown.style.display === "none") {
-        tzPopdown.style.display = "block";
-        arrow.src = up_arrow;
-        setTimes();
-    } else {
-        tzPopdown.style.display = "none";
-        arrow.src = down_arrow;
-    }
-}
-
-function getTimeStr() {
+function getCurrentTimeString() {
     var now = new Date;
     var hour = now.getHours();
     if (hour <= 12) {
@@ -21,8 +8,25 @@ function getTimeStr() {
     }
 }
 
-function setTimes() {
-    const pacific = document.getElementById('pacific-time');
-    var now = new Date;
-    pacific.children[1].textContent = now.toLocaleTimeString('en-US',{ hour: '2-digit', minute: '2-digit' });
-}
+function formatOption(option) {
+    if (!option.id) {
+        return option.text;
+    }
+    var currentTime = getCurrentTimeString();
+    var formattedOption = $('<span style="display: flex; justify-content: space-between;">' +
+    '<span style="flex: 1;">' + option.text + '</span>' +
+    '<span>' + currentTime + '</span>' +
+    '</span>');
+    return formattedOption;
+  }
+
+
+// select2 setup
+$(document).ready(function() {
+    $('.js-example-basic-single').select2({
+        dropdownCssClass: 'dropdown',
+        selectionCssClass: 'searchbox',
+        width: "494px",
+        templateResult: formatOption
+    });
+});
